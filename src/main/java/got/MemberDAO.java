89e4aff713f793be347e6245fbdd7177043d5bde
Kenneth Kousen <ken.kousen@kousenit.com>
2017-03-06 18:35:25 -0500

    Remove author comment

--- src/main/java/got/MemberDAO.java
+++ src/main/java/got/MemberDAO.java
@@ -4,9 +4,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Created by Ken Kousen on 7/18/16.
- */
 public interface MemberDAO {
     Member findById(Long id);
     Optional<Member> findByName(String name);
