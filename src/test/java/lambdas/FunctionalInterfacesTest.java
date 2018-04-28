b5aa3b68788c06db502a323d229d495a70aa8368
Ken Kousen <ken.kousen@kousenit.com>
2018-04-28 17:06:24 +0530

    Add solutions for workshop at GIDS 2018

--- src/test/java/lambdas/FunctionalInterfacesTest.java
+++ src/test/java/lambdas/FunctionalInterfacesTest.java
@@ -14,7 +14,7 @@ public class FunctionalInterfacesTest {
 
     @Test
     public void implementConsumerUsingAnonInnerClass() throws Exception {
-        Consumer<String> consumer = new Consumer<>() {
+        Consumer<String> consumer = new Consumer<String>() {
             @Override
             public void accept(String s) {
                 System.out.println(s);
@@ -26,35 +26,46 @@ public class FunctionalInterfacesTest {
     @SuppressWarnings("Convert2MethodRef")
     @Test
     public void implementConsumerUsingLambda() throws Exception {
+        Consumer<String> consumer = string -> System.out.println(string);
+        consumer.accept("Hello, World!");
     }
 
     @Test
     public void implementConsumerUsingMethodReference() throws Exception {
+        Consumer<String> consumer = System.out::println;
+        consumer.accept("Hello, World!");
     }
 
     @Test
     public void implementSupplierUsingAnonInnerClass() throws Exception {
-
-        //        assertEquals("Hello", supplier.get());
+        Supplier<String> supplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                return "Hello";
+            }
+        };
+        assertEquals("Hello", supplier.get());
     }
 
     @Test
     public void implementSupplierUsingLambda() throws Exception {
-
-//        assertEquals("Hello", supplier.get());
+        Supplier<String> supplier = () -> "Hello";
+        assertEquals("Hello", supplier.get());
     }
 
     @Test
     public void implementSupplierUsingMethodReference() throws Exception {
         // Create a Supplier<Double> that calls Math.random()
 
-//        assertTrue(supplier.get() >= 0.0);
-//        assertTrue(supplier.get() <= 1.0);
+        Supplier<Double> supplier = Math::random;
+        assertTrue(supplier.get() >= 0.0);
+        assertTrue(supplier.get() <= 1.0);
 
         // Create a DoubleSupplier that does the same
 
-//        assertTrue(doubleSupplier.getAsDouble() >= 0.0);
-//        assertTrue(doubleSupplier.getAsDouble() <= 1.0);
+        DoubleSupplier doubleSupplier = Math::random;
+        assertTrue(doubleSupplier.getAsDouble() >= 0.0);
+        assertTrue(doubleSupplier.getAsDouble() <= 1.0);
     }
 
     @Test
@@ -65,21 +76,28 @@ public class FunctionalInterfacesTest {
 
         // Add the strings to a Set
 
-//        assertEquals(4, strings.size());
-//        assertEquals(HashSet.class, strings.getClass());
+        Set<String> strings = stringList.stream()
+                .collect(Collectors.toSet());
+        assertEquals(4, strings.size());
+        assertEquals(HashSet.class, strings.getClass());
 
         // Add the strings to a TreeSet
+        SortedSet<String> sortedStrings =
+                stringList.stream()
+                          .collect(Collectors.toCollection(TreeSet::new));
 
-//        assertEquals(4, sortedStrings.size());
-//        assertEquals(TreeSet.class, sortedStrings.getClass());
-//        assertEquals("a", sortedStrings.first());
+        assertEquals(4, sortedStrings.size());
+        assertEquals(TreeSet.class, sortedStrings.getClass());
+        assertEquals("a", sortedStrings.first());
 
     }
 
     @Test
     public void filterWithPredicate() throws Exception {
-//        IntStream.of(3, 1, 4, 1, 5, 9)
-//                .filter(n -> true)  // accept even nums only
-//                .forEach(n -> assertTrue(n % 2 == 0));
+        IntPredicate evens = n -> n % 2 == 0;
+
+        IntStream.of(3, 1, 4, 1, 5, 9)
+                .filter(evens)  // accept even nums only
+                .forEach(n -> assertTrue(n % 2 == 0));
     }
 }
