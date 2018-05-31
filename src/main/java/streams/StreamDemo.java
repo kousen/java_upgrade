6cd34df7d23e683c601996b1584c5605538f9e6c
Ken Kousen <ken.kousen@kousenit.com>
2018-05-31 10:51:42 -0400

    Add stream iterator and reduce demo

--- src/main/java/streams/StreamDemo.java
+++ src/main/java/streams/StreamDemo.java
@@ -1,10 +1,12 @@
 package streams;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void main(String[] args) {
@@ -21,7 +23,7 @@ public class StreamDemo {
         List<Integer> nums = Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5);
 
         List<Integer> collected = nums.parallelStream()
-                                    .collect(Collectors.toList());
+                                      .collect(Collectors.toList());
         // collect respects encounter order -- always get the same order back
         System.out.println(collected);
 
@@ -29,5 +31,22 @@ public class StreamDemo {
         nums.parallelStream()
             .forEach(results::add); // does NOT care about encounter order
         System.out.println(results);
+
+        BigDecimal two = new BigDecimal("2.0");
+        BigDecimal sum = Stream.iterate(BigDecimal.ONE, n -> n.add(BigDecimal.ONE))
+                               .limit(10)
+                               .reduce(BigDecimal.ZERO,
+                                       (accumulator, val) -> {
+                                   System.out.println("accumulator = " +
+                                    accumulator + ", val = " + val);
+                                   return accumulator.add(val.multiply(two));
+                               });
+        System.out.println("The sum of twice the first 10 big decimals is " + sum);
+
+        sum = Stream.iterate(BigDecimal.ONE, n -> n.add(BigDecimal.ONE))
+                               .limit(10)
+                               .reduce(BigDecimal.ZERO,
+                                       (accumulator, val) -> accumulator.add(val.multiply(two)));
+        System.out.println("The sum of the first 10 big decimals is " + sum);
     }
 }
