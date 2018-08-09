11fbbddc52d032fecf79003aadf73fd80cd6498f
Ken Kousen <ken.kousen@kousenit.com>
2018-08-09 12:04:30 -0400

    Implement company-employee example

--- src/main/java/sorting/SortGolfers.java
+++ src/main/java/sorting/SortGolfers.java
@@ -9,24 +9,24 @@ import static java.util.Comparator.comparingInt;
 public class SortGolfers {
     private List<Integer> integers = Arrays.asList(3, 1, 4, 1, 5, 9);
 
-    public List<Integer> getValuesBefore() {
-        List<Integer> results = new ArrayList<>();
-        for (Integer i : integers) {
-            int val = i * 2;
-            if (val % 3 == 0) {
-                results.add(i);
-            }
-        }
-        return results;
-    }
-
-    public List<Integer> getValuesAfter() {
-        return integers.stream()
-                .mapToInt(n -> n * 2)
-                .filter(n -> n % 3 == 0)
-                .boxed()
-                .collect(Collectors.toList());
-    }
+//    public List<Integer> getValuesBefore() {
+//        List<Integer> results = new ArrayList<>();
+//        for (Integer i : integers) {
+//            int val = i * 2;
+//            if (val % 3 == 0) {
+//                results.add(i);
+//            }
+//        }
+//        return results;
+//    }
+//
+//    public List<Integer> getValuesAfter() {
+//        return integers.stream()
+//                .mapToInt(n -> n * 2)
+//                .filter(n -> n % 3 == 0)
+//                .boxed()
+//                .collect(Collectors.toList());
+//    }
 
     private static List<Golfer> golfers = Arrays.asList(
             new Golfer("Jack", "Nicklaus", 68),
@@ -43,8 +43,8 @@ public class SortGolfers {
     }
 
     public static void main(String[] args) {
-        Golfer nicklaus = golfers.get(0);
-        System.out.println(nicklaus);
+//        Golfer nicklaus = golfers.get(0);
+//        System.out.println(nicklaus);
 
         // sort by score
         SortGolfers.printSorted(comparingInt(Golfer::getScore));
@@ -68,17 +68,17 @@ public class SortGolfers {
                 .collect(Collectors.toList());
 
         // The sort still maintains references to the same objects
-        System.out.println(sorted.get(0) == nicklaus);
+//        System.out.println(sorted.get(0) == nicklaus);
 
         // Copy, then sort the copies
-        System.out.println("Sorting the copies:");
-        sorted = golfers.stream()
-                .map(Golfer::new)  // Make new golfers that are copies of the originals
-                .sorted(comparing(Golfer::getScore)
-                        .thenComparing(Golfer::getLast)
-                        .thenComparing(Golfer::getFirst))
-                .collect(Collectors.toList());
-        System.out.println(sorted.get(0) == nicklaus);
+//        System.out.println("Sorting the copies:");
+//        sorted = golfers.stream()
+//                .map(Golfer::new)  // Make new golfers that are copies of the originals
+//                .sorted(comparing(Golfer::getScore)
+//                        .thenComparing(Golfer::getLast)
+//                        .thenComparing(Golfer::getFirst))
+//                .collect(Collectors.toList());
+//        System.out.println(sorted.get(0) == nicklaus);
 
         // Partition Golfers into two categories: above and below score = 70
         Map<Boolean, List<Golfer>> map = golfers.stream()
