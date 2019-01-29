b8f22011f1f8a0a7fe57e9c35f0e01c9a8fe167c
Ken Kousen <ken.kousen@kousenit.com>
2019-01-29 10:58:06 -0500

    Add deferred execution demo using Product

---
+++ src/main/java/optional/Product.java
@@ -0,0 +1,64 @@
+package optional;
+
+import java.util.Objects;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private double price;
+
+    public Product(Integer id, String name, double price) {
+        System.out.println("Inside Product constructor with id=" + id);
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 &&
+                Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
---
+++ src/main/java/optional/ProductDAO.java
@@ -0,0 +1,25 @@
+package optional;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.Map.entry;
+import static java.util.Map.ofEntries;
+
+public class ProductDAO {
+    private static Map<Integer, Product> products =
+            ofEntries(entry(1, new Product(1, "Football", 12.99)),
+                    entry(2, new Product(2, "Basketball", 15.99)),
+                    entry(3, new Product(3, "Baseball", 5.99)));
+
+    public Optional<Product> findById(Integer id) {
+        return Optional.ofNullable(products.get(id));
+    }
+
+
+    public Product getProductById(Integer id) {
+        Optional<Product> optional = findById(id);
+        // Only create default product if there is not product with that id
+        return optional.orElseGet(() -> new Product(999, "No name", 0.00));
+    }
+}
