38bae9867302edf781dc5d7bb02a7f2f9e92f764
Ken Kousen <ken.kousen@kousenit.com>
2019-04-23 09:09:08 -0400

    Add more exercise solutions

--- src/test/java/streams/StringExercises.java
+++ src/test/java/streams/StringExercises.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import java.awt.*;
 import java.util.*;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
@@ -71,12 +73,40 @@ public class StringExercises {
     public void demoCollectors() {
         // Get only strings of even length
         // Add them to a LinkedList
+        LinkedList<String> evens = strings.stream()
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toCollection(LinkedList::new));
+        System.out.println(evens);
 
         // Add the strings to a map of string to length
+        Map<String, Integer> map = strings.stream()
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toMap(s -> s, String::length));
+
+        map.forEach((word, len) -> System.out.println(word + ": " + len));
 
         // Filter out nulls, then print even-length strings
+        List<String> stringsWithNulls = Arrays.asList("this", null, "is",
+                null, "a", "list", null, "of", "strings", null, "with", "nulls");
+
+        List<String> evenLengths = stringsWithNulls.stream()
+//                .filter(s -> s != null && s.length() % 2 == 0)
+                .filter(Objects::nonNull)
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(evenLengths);
 
         // Combine the two predicates and use the result to print non-null, even-length strings
+        Predicate<String> nonNulls = Objects::nonNull;
+        Predicate<String> evenLength = s -> s.length() % 2 == 0;
+        stringsWithNulls.stream()
+                .filter(nonNulls.and(evenLength))
+                .forEach(System.out::println);
+
+        Logger logger = Logger.getLogger("MyLogger");
+        Consumer<String> log = logger::info;
+        Consumer<String> printer = System.out::println;
+        stringsWithNulls.forEach(log.andThen(printer));
     }
 
 }
