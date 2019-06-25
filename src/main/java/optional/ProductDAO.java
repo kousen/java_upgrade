cd53dc5fa84e2492eb34a55a598a341413438bf1
Ken Kousen <ken.kousen@kousenit.com>
2019-06-25 10:14:57 -0400

    Add lambda example

--- src/main/java/optional/ProductDAO.java
+++ src/main/java/optional/ProductDAO.java
@@ -3,17 +3,18 @@ package optional;
 import java.util.Map;
 import java.util.Optional;
 
-import static java.util.Map.entry;
-import static java.util.Map.ofEntries;
+//import static java.util.Map.entry;
+//import static java.util.Map.ofEntries;
 
 public class ProductDAO {
-    private static Map<Integer, Product> products =
-            ofEntries(entry(1, new Product(1, "Football", 12.99)),
-                    entry(2, new Product(2, "Basketball", 15.99)),
-                    entry(3, new Product(3, "Baseball", 5.99)));
+//    private static Map<Integer, Product> products =
+//            ofEntries(entry(1, new Product(1, "Football", 12.99)),
+//                    entry(2, new Product(2, "Basketball", 15.99)),
+//                    entry(3, new Product(3, "Baseball", 5.99)));
 
     public Optional<Product> findById(Integer id) {
-        return Optional.ofNullable(products.get(id));
+//        return Optional.ofNullable(products.get(id));
+        return Optional.empty();
     }
 
 
---
+++ src/test/java/lambdas/FileFilterTest.java
@@ -0,0 +1,47 @@
+package lambdas;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileFilter;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileFilterTest {
+    private File root = new File("src/main/java");
+
+    @Test
+    public void listAllFiles() {
+        File[] files = root.listFiles();
+
+        for (File file : files) {
+            System.out.println(file);
+        }
+        assertEquals(19, files.length);
+    }
+
+    @Test
+    public void listDirectories_anonInnerClass() {
+        // Java 5, 6, 7
+        File[] files = root.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();
+            }
+        });
+        assertEquals(11, files.length);
+    }
+
+    @Test
+    public void listDirectories_lambda() {
+        File[] files = root.listFiles(pathname -> pathname.isDirectory());
+        assertEquals(11, files.length);
+    }
+
+    @Test
+    public void listDirectories_methodReference() {
+        File[] files = root.listFiles(File::isDirectory);
+        assertEquals(11, files.length);
+    }
+
+}
