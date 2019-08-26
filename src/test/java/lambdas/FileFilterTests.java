1e27a1f76cc1c48216bfe01e78c8b1b1b78ad488
Ken Kousen <ken.kousen@kousenit.com>
2019-08-26 11:07:29 -0400

    Add FilenameFilter demo and some function interface examples

--- src/test/java/lambdas/FileFilterTests.java
+++ src/test/java/lambdas/FileFilterTests.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -53,5 +58,27 @@ public class FileFilterTests {
         File root = new File("src/main/java");
         File[] files = root.listFiles(File::isDirectory);
         assertEquals(11, files.length);
+    }
+
+    @Test
+    public void fileNameFilterShowJavaFiles() {
+        File root = new File("src/main/java");
+        File[] files = root.listFiles((directory, fileName) -> fileName.endsWith(".java"));
+//        for (File file : files) {
+//            System.out.println(file);
+//        }
+        assertEquals(8, files.length);
+
+        Stream.of(files).forEach(f -> System.out.println(f));
+
+        List<String> stringList = Arrays.asList("first", "second", "third");
+        stringList.forEach(System.out::println);
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("first", 1);
+        map.put("second", 2);
+        map.put("third", 2);
+
+        map.forEach((k, v) -> System.out.println(k + " maps to " + v));
     }
 }
