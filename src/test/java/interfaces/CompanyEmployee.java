6edcd29cafdec4782819435c10beab85ceda3849
Ken Kousen <ken.kousen@kousenit.com>
2019-12-19 11:50:06 -0500

    Solve default methods exercise

---
+++ src/test/java/interfaces/CompanyEmployee.java
@@ -0,0 +1,32 @@
+package interfaces;
+
+public class CompanyEmployee implements Company, Employee {
+    private String first;
+    private String last;
+
+    public CompanyEmployee(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public String getName() {
+        return Employee.super.getName() + " works for " +
+                Company.super.getName();
+    }
+
+    @Override
+    public String getFirst() {
+        return first;
+    }
+
+    @Override
+    public String getLast() {
+        return last;
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println("Converting caffeine to code for money...");
+    }
+}
