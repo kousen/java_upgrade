b174e50160a04c7cc2a4c2594e1af3fdb002d823
Ken Kousen <ken.kousen@kousenit.com>
2020-06-25 10:09:15 -0400

    Add LazyStreams demo

--- src/main/java/lazy/LazyStreams.java
+++ src/main/java/lazy/LazyStreams.java
@@ -20,17 +20,20 @@ public class LazyStreams {
     public static void main(String[] args) {
         // multiply numbers between 100 and 200 by 2, then find first n divisible by 3
         int firstEvenDoubleDivBy3 = IntStream.range(100, 200)
-                .map(n -> n * 2)
+                .peek(n -> System.out.println("The current value of n is " + n))
                 .filter(n -> n % 3 == 0)
-                .findFirst().orElse(0);
+                .peek(n -> System.out.println("After filter, n is " + n))
+                .map(n -> n * 2)
+                .peek(n -> System.out.println("After map, n is " + n))
+                .findFirst().orElse(-1);
         System.out.println(firstEvenDoubleDivBy3);
 
 
         // Demonstrate laziness using print statements
         firstEvenDoubleDivBy3 = IntStream.range(100, 2_000_000)
-                .map(LazyStreams::multByTwo)
                 .filter(LazyStreams::modByThree)
-                .findFirst().orElse(0);
+                .map(LazyStreams::multByTwo)
+                .findFirst().orElse(-1);
         System.out.printf("First even divisible by 3 is %d%n", firstEvenDoubleDivBy3);
     }
 }
