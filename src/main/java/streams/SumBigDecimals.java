aad82371a5fab2e8788eb74910c365593f773d6d
Ken Kousen <ken.kousen@kousenit.com>
2020-07-24 12:06:58 -0400

    Add comments to reduce example

--- src/main/java/streams/SumBigDecimals.java
+++ src/main/java/streams/SumBigDecimals.java
@@ -16,7 +16,10 @@ public class SumBigDecimals {
     public BigDecimal sumFirstN_usingReduce(int n) {
         return Stream.iterate(BigDecimal.ONE, bd -> bd.add(BigDecimal.ONE))
                 .limit(n)
-                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+                .reduce((a, b) -> {
+                    System.out.println("a = " + a + ", b = " + b);
+                    return a.add(b);
+                }).orElse(BigDecimal.ZERO);
     }
 
     // Off by one error, because 1 is never doubled
