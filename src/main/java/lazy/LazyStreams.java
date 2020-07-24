0d0cb2645edf4ddc469e929b89d37c4b145a21ad
Ken Kousen <ken.kousen@kousenit.com>
2020-07-24 11:00:58 -0400

    Use peek inside lazy streams

--- src/main/java/lazy/LazyStreams.java
+++ src/main/java/lazy/LazyStreams.java
@@ -20,7 +20,9 @@ public class LazyStreams {
     public static void main(String[] args) {
         // multiply numbers between 100 and 200 by 2, then find first n divisible by 3
         int firstEvenDoubleDivBy3 = IntStream.range(100, 200)
+                .peek(n -> System.out.println("Before filter: n = " + n))
                 .filter(n -> n % 3 == 0)
+                .peek(n -> System.out.println("After filter: n = " + n))
                 .map(n -> n * 2)
                 .findFirst().orElse(0);
         System.out.println(firstEvenDoubleDivBy3);
