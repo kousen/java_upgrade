e9e7dabe3d6ba720f32489cb4f5895e435cd0c02
Ken Kousen <ken.kousen@kousenit.com>
2020-08-17 12:02:16 -0400

    Add solution to string exercises

--- src/test/java/streams/StringExercises.java
+++ src/test/java/streams/StringExercises.java
@@ -9,6 +9,10 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparingInt;
+import static java.util.Comparator.naturalOrder;
+import static java.util.stream.Collectors.toList;
+
 public class StringExercises {
     private final List<String> strings = Arrays.asList("this", "is", "a",
             "list", "of", "strings");
@@ -27,36 +31,89 @@ public class StringExercises {
     @Test
     public void stringLengthSort_lambda() {
         // Use lambda for the Comparator (reverse sort)
-
+        strings.sort((s1, s2) -> s2.length() - s1.length());
+        System.out.println(strings);
         // Use the "sorted" method on Stream
+        List<String> sorted = strings.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length())
+                .collect(toList());
+        System.out.println(sorted);
+        System.out.println(strings);
     }
 
-    private static int compareStrings(String s1, String s2) {
+    private int compareStrings(String s1, String s2) {
         return s1.length() - s2.length();
     }
 
     @Test  // Use a lambda that calls 'compareStrings' directly
     public void stringLengthSort_methodCall() {
+        List<String> sorted = strings.stream()
+                .sorted((s1, s2) -> compareStrings(s1, s2))
+                .collect(toList());
+        System.out.println(sorted);
     }
 
     @Test  // Use a method ref to 'compareStrings'
     public void stringLengthSort_methodRef() {
+        List<String> sorted = strings.stream()
+                .sorted(this::compareStrings)
+                .collect(toList());
+        System.out.println(sorted);
     }
 
     @Test  // Use Comparator.comparingInt
     public void stringLengthSort_comparingInt() {
+        List<String> sorted = strings.stream()
+                .sorted(comparingInt(String::length)
+                        .thenComparing(naturalOrder()))
+                .collect(toList());
+        System.out.println(sorted);
+        System.out.println(sorted.getClass().getName());
     }
 
     @Test
     public void demoCollectors() {
         // Get only strings of even length
         // Add them to a LinkedList
+        LinkedList<String> evens = strings.stream()
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toCollection(LinkedList::new));
+        System.out.println(evens);
 
         // Add the strings to a map of string to length
+        Map<String, Integer> map = strings.stream()
+                // .collect(Collectors.toMap(s -> s, String::length));
+                .collect(Collectors.toMap(Function.identity(), String::length));
+        map.forEach((string,length) -> System.out.println(string + ": " + length));
 
         // Filter out nulls, then print even-length strings
+        List<String> stringsWithNulls = Arrays.asList(null, "this", null, "is", "a",
+                null, "list", "of", null, "strings", "with", null, "nulls");
+
+        List<String> evenLengths = stringsWithNulls.stream()
+                .filter(s -> s != null && s.length() % 2 == 0)
+                .collect(toList());
+        System.out.println(evenLengths);
+
+        evenLengths = stringsWithNulls.stream()
+                .filter(s -> s != null)
+                .filter(s -> s.length() % 2 == 0)
+                .collect(toList());
+        System.out.println(evenLengths);
+
+        evenLengths = stringsWithNulls.stream()
+                .filter(Objects::nonNull)
+                .filter(s -> s.length() % 2 == 0)
+                .collect(toList());
+        System.out.println(evenLengths);
 
         // Combine the two predicates and use the result to print non-null, even-length strings
+        Predicate<String> nonNull = Objects::nonNull;
+        Predicate<String> evenLength = s -> s.length() % 2 == 0;
+        evenLengths = stringsWithNulls.stream()
+                .filter(nonNull.and(evenLength))
+                .collect(toList());
+        System.out.println(evenLengths);
     }
 
 }
