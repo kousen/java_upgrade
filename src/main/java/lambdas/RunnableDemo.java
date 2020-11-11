0beecf3653b04f6b7da9e43affeb9e62cd1ada2e
Ken Kousen <ken.kousen@kousenit.com>
2020-11-11 08:35:32 -0500

    Add comments to runnable demo

--- src/main/java/lambdas/RunnableDemo.java
+++ src/main/java/lambdas/RunnableDemo.java
@@ -10,12 +10,15 @@ public class RunnableDemo {
             }
         }).start();
 
+        // Expression lambda
         new Thread(() -> System.out.println("Inside expression lambda")).start();
 
+        // Block lambda
         new Thread(() -> {
             System.out.println("Inside block lambda");
         }).start();
 
+        // Assign a lambda to a variable
         Runnable runnable = () -> System.out.println("Assigned to a variable");
         new Thread(runnable).start();
     }
