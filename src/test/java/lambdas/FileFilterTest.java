0b6c74abd9a28ec8d554967dd3e88c9193b419d5
Ken Kousen <ken.kousen@kousenit.com>
2021-06-30 11:59:21 -0400

    Remove unneeded imports

--- src/test/java/lambdas/FileFilterTest.java
+++ src/test/java/lambdas/FileFilterTest.java
@@ -6,8 +6,6 @@ import java.io.File;
 import java.io.FileFilter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
