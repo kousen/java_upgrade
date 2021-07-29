af20c10311c8d8723ce695596f70ec30209ae9ea
Ken Kousen <ken.kousen@kousenit.com>
2021-07-29 10:52:53 -0400

    Add StringExercises solutions

--- src/test/java/streams/StringExercises.java
+++ src/test/java/streams/StringExercises.java
@@ -3,10 +3,13 @@ package streams;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.nio.file.DirectoryStream;
 import java.util.*;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class StringExercises {
@@ -15,7 +18,7 @@ public class StringExercises {
 
     @Test
     public void stringLengthSort_InnerClass() {     // Java 5, 6, 7
-        Collections.sort(strings, new Comparator<String>() {
+        strings.sort(new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
                 return s1.length() - s2.length();
@@ -27,36 +30,83 @@ public class StringExercises {
     @Test
     public void stringLengthSort_lambda() {
         // Use lambda for the Comparator (reverse sort)
-
+        strings.sort((s1, s2) -> s2.length() - s1.length());
+        System.out.println(strings);
         // Use the "sorted" method on Stream
+        List<String> sorted = strings.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length())
+                .collect(Collectors.toList());
+        System.out.println(sorted);
     }
 
-    private static int compareStrings(String s1, String s2) {
+    private int compareStrings(String s1, String s2) {
         return s1.length() - s2.length();
     }
 
     @Test  // Use a lambda that calls 'compareStrings' directly
     public void stringLengthSort_methodCall() {
+        List<String> sorted = strings.stream()
+                .sorted((s1, s2) -> compareStrings(s1, s2))
+                .collect(Collectors.toList());
+        System.out.println(sorted);
     }
 
     @Test  // Use a method ref to 'compareStrings'
     public void stringLengthSort_methodRef() {
+        List<String> sorted = strings.stream()
+                .sorted(this::compareStrings)
+                .collect(Collectors.toList());
+        System.out.println(sorted);
     }
 
     @Test  // Use Comparator.comparingInt
     public void stringLengthSort_comparingInt() {
+        List<String> sorted = strings.stream()
+                //.sorted(Comparator.comparingInt(s -> s.length()))
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+        System.out.println(sorted);
     }
 
     @Test
     public void demoCollectors() {
         // Get only strings of even length
         // Add them to a LinkedList
+        LinkedList<String> evens = strings.stream()
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toCollection(LinkedList::new));
+        System.out.println(evens);
 
         // Add the strings to a map of string to length
+        Map<String, Integer> map = strings.stream()
+                // .collect(Collectors.toMap(s -> s, String::length));
+                .collect(Collectors.toMap(Function.identity(), String::length));
+        System.out.println(map);
 
         // Filter out nulls, then print even-length strings
+        List<String> stringsWithNulls = Arrays.asList(null, "this", null,
+                "is", "a", null, "list");
+        List<String> result = stringsWithNulls.stream()
+                // .filter(s -> s != null && s.length() % 2 == 0) // short-circuiting &&
+                .filter(Objects::nonNull)
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(result);
 
         // Combine the two predicates and use the result to print non-null, even-length strings
+        Predicate<String> evenLength = (String s) -> s.length() % 2 == 0;
+        Predicate<String> nonNull = Objects::nonNull;
+        result = stringsWithNulls.stream()
+                .filter(nonNull.and(evenLength))  // Function composition
+                .collect(Collectors.toList());
+        System.out.println(result);
+
+        Logger logger = Logger.getLogger("MyLogger");
+        Consumer<String> log = logger::info;
+        Consumer<String> out = System.out::println;
+        // Deeply evil, but works
+        //result.forEach(((Consumer<String>) logger::info).andThen(System.out::println));
+        result.forEach(log.andThen(out));
     }
 
 }
