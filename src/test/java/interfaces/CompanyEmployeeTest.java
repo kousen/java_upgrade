bd99b96303d76f93649183a804910a7c7e22a363
Ken Kousen <ken.kousen@kousenit.com>
2021-08-05 11:59:38 -0400

    Solve company-employee example and add supplier demos

--- src/test/java/interfaces/CompanyEmployeeTest.java
+++ src/test/java/interfaces/CompanyEmployeeTest.java
@@ -2,6 +2,11 @@ package interfaces;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // Create a class called CompanyEmployee that implements both
 //   the Company and Employee interfaces
 // Implement the necessary methods
@@ -11,7 +16,20 @@ public class CompanyEmployeeTest {
 
     @Test
     public void getName() {
-//        CompanyEmployee emp = new CompanyEmployee("Peter", "Gibbons");
-//        assertEquals("Peter Gibbons works for Initech", emp.getName());
+        CompanyEmployee emp = new CompanyEmployee("Peter", "Gibbons");
+        assertEquals("Peter Gibbons works for Initech", emp.getName());
+    }
+
+    @Test
+    void forEachOnIterable() {
+        List.of(new CompanyEmployee("Peter", "Gibbons"),
+                new CompanyEmployee("Michael", "Bolton"),
+                new CompanyEmployee("Bill", "Lumbergh"))
+                .forEach(System.out::println);
+
+        Map.ofEntries(Map.entry("a", 1),
+                Map.entry("b", 2),
+                Map.entry("c", 2))
+                .forEach((k,v) -> System.out.println(k + " maps to " + v));
     }
 }
--- src/test/java/lambdas/LazyErrorMessage.java
+++ src/test/java/lambdas/LazyErrorMessage.java
@@ -2,8 +2,12 @@ package lambdas;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LazyErrorMessage {
@@ -16,7 +20,31 @@ public class LazyErrorMessage {
 
     @Test
     void showError() {
-        assertTrue(true, () -> getErrorMessage());
+        // assertTrue(true, () -> getErrorMessage());
+        assertTrue(true, this::getErrorMessage);
         log.fine(() -> "Error messsage");  // Creates the log message only if it will be seen
+    }
+
+    @Test
+    void effectivelyFinal() {
+        int total = 0;  // "effectively" final
+//        Stream.of(3, 1, 4, 1, 5, 9)
+//                .forEach(n -> total += n);  // same rule as with anon inner classes
+        total = IntStream.of(3, 1, 4, 1, 5, 9).sum();
+    }
+
+    @Test
+    void NPE() {
+        String str = null;
+        assertThrows(NullPointerException.class, () -> str.length());
+        new Thread(() -> dealWithIOException("-1"));
+    }
+
+    private void dealWithIOException(String arg) {
+        try {
+            throw new IOException(arg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
