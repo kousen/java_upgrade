73c76d1ce1ab596613b4ca1007979dbf0f1c9e67
Ken Kousen <ken.kousen@kousenit.com>
2021-08-06 11:00:40 -0400

    Add summary statistics demo

---
+++ src/main/java/streams/SummaryStatistics.java
@@ -0,0 +1,25 @@
+package streams;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
+
+public class SummaryStatistics {
+    public static void main(String[] args) {
+        DoubleSummaryStatistics stats = DoubleStream.generate(Math::random)
+                .limit(1_000_000)
+                .summaryStatistics();
+        System.out.println(stats);
+
+        System.out.println("Min: " + stats.getMin());
+        System.out.println("Ave: " + stats.getAverage());
+        System.out.println("Max: " + stats.getMax());
+        System.out.println("Count: " + stats.getCount());
+        System.out.println("Sum: " + stats.getSum());
+
+        OptionalDouble max = DoubleStream.generate(Math::random)
+                .limit(1_000_000)
+                .max();
+        System.out.println("The max value is " + max);
+    }
+}
