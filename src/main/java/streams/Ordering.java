4f3c191cae0f3151f661ed9c5b9bd5779ed2ab48
Ken Kousen <ken.kousen@kousenit.com>
2021-09-30 11:19:12 -0400

    Switch to Java 8

--- src/main/java/streams/Ordering.java
+++ src/main/java/streams/Ordering.java
@@ -1,12 +1,13 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Ordering {
     public static void main(String[] args) {
-        List<Integer> ints = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         // Collect doubles into a generated list
         List<Integer> doubles = ints.stream()
