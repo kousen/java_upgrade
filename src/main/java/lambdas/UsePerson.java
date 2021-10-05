e080d6f6a03a68de4e359869c3d9b424ae872343
Ken Kousen <ken.kousen@kousenit.com>
2021-10-05 11:55:46 -0600

    Add tests and person demo

--- src/main/java/lambdas/UsePerson.java
+++ src/main/java/lambdas/UsePerson.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,21 +10,26 @@ public class UsePerson {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("John", "Paul", "George", "Ringo");
 
+        // Java 7 and earlier
+        List<Person> beatles = new ArrayList<>();  // Shared mutable state
+        for (String name : names) {
+            beatles.add(new Person(name));
+        }
+        System.out.println(beatles);
+
         List<Person> people = names.stream()    // Stream<String>
                 .map(name -> new Person(name))  // Stream<Person>
                 .collect(Collectors.toList());  // Converts Stream<Person> to List<Person>
-
+        System.out.println(people);
 
         people = names.stream()
                 .map(Person::new)
                 .collect(Collectors.toList());
-
         System.out.println(people);
 
         Person[] peopleArray = names.stream()
                 .map(Person::new)
                 .toArray(Person[]::new);
-
         System.out.println(Arrays.toString(peopleArray));
     }
 }
--- src/test/java/lambdas/FileFilterTest.java
+++ src/test/java/lambdas/FileFilterTest.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class FileFilterTest {
     private final File root = new File("src/main/java");
@@ -54,10 +60,46 @@ public class FileFilterTest {
 
     @Test
     public void listDirectories_variable() {
-        FileFilter filter = pathname -> pathname.isDirectory();
+        // FileFilter filter = pathname -> pathname.isDirectory();
+        FileFilter filter = File::isDirectory;
         File[] directories = root.listFiles(filter);
         if (directories != null) {
             assertEquals(13, directories.length);
         }
+    }
+
+    @Test
+    public void listJavaSourceFiles() {
+        File[] javaSrc = root.listFiles((dir, name) -> name.endsWith(".java"));
+        if (javaSrc != null) {
+            assertEquals(8, javaSrc.length);
+        }
+    }
+
+    @Test
+    void forEachConsumerOfList() {
+        List<String> strings = Arrays.asList("this", "is", "a", "list", "of", "strings");
+        strings.forEach(x -> System.out.println(x));
+        strings.forEach(System.out::println);
+    }
+
+    @Test
+    void forEachConsumerOfMap() {
+        //Map<String, Integer> map = Map.ofEntries(Map.entry("a", 1), Map.entry("b", 2), Map.entry("c", 2));
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1); map.put("b", 2); map.put("c", 2);
+        map.forEach((key, value) -> System.out.println(key + " map to " + value));
+    }
+
+    private String getErrorMessage() {
+        System.out.println("Inside getErrorMessage");
+        return "error message";
+    }
+
+    @Test // Lazy or deferred evaluation
+    void notNullSupplier() {
+        String s = "abc";
+        assertNotNull(s, () -> getErrorMessage());
+        assertNotNull(s, this::getErrorMessage);
     }
 }
