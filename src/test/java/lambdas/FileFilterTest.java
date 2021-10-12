d9d0b4c95e05e979a12b61c0b489af8479ad6a18
Ken Kousen <ken.kousen@kousenit.com>
2021-10-12 09:00:38 -0400

    Add FileFilterTest

---
+++ src/test/java/lambdas/FileFilterTest.java
@@ -0,0 +1,56 @@
+package lambdas;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileFilter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileFilterTest {
+    private final File root = new File("src/main/java");
+
+    @Test
+    public void listAllFiles() {
+        File[] files = root.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file);
+            }
+            assertEquals(21, files.length);
+        }
+    }
+
+    @Test
+    public void listDirectories_anonInnerClass() {
+        File[] directories = root.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();
+            }
+        });
+        if (directories != null) {
+            assertEquals(13, directories.length);
+        }
+    }
+
+    @SuppressWarnings("Convert2MethodRef")
+    @Test
+    public void listDirectories_expressionLambda() {
+        File[] directories = root.listFiles(pathname -> pathname.isDirectory());
+        if (directories != null) {
+            assertEquals(13, directories.length);
+        }
+    }
+
+    @SuppressWarnings("Convert2MethodRef")
+    @Test
+    public void listDirectories_blockLambda() {
+        File[] directories = root.listFiles(pathname -> {
+            return pathname.isDirectory();
+        });
+        if (directories != null) {
+            assertEquals(13, directories.length);
+        }
+    }
+}
