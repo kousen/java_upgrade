d6cff62882a982e3fecf0cb46672ac209adf19e1
Ken Kousen <ken.kousen@kousenit.com>
2021-10-12 11:34:46 -0400

    Add exercise on default methods

--- src/main/java/lambdas/UsePerson.java
+++ src/main/java/lambdas/UsePerson.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,21 +10,29 @@ public class UsePerson {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("John", "Paul", "George", "Ringo");
 
+        // Java 7 (or 6 or even 5)
+        List<Person> beatles = new ArrayList<>();  // shared mutable state
+        for (String name : names) {
+            beatles.add(new Person(name));
+        }
+        System.out.println(beatles);
+
+        // Java 8+ stream
         List<Person> people = names.stream()    // Stream<String>
                 .map(name -> new Person(name))  // Stream<Person>
                 .collect(Collectors.toList());  // Converts Stream<Person> to List<Person>
-
-
-        people = names.stream()
-                .map(Person::new)
-                .collect(Collectors.toList());
-
         System.out.println(people);
 
+        // Constructor reference
+        people = names.stream()
+                .map(Person::new) // map converts String to Person
+                .collect(Collectors.toList());
+        System.out.println(people);
+
+        // Stream also has a toArray method
         Person[] peopleArray = names.stream()
                 .map(Person::new)
                 .toArray(Person[]::new);
-
         System.out.println(Arrays.toString(peopleArray));
     }
 }
