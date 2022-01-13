3740f921688a6fa3d67e2fb5d76174b6a39954e8
Ken Kousen <ken.kousen@kousenit.com>
2022-01-13 14:04:12 -0500

    Show lazy streams and random demo

--- src/main/java/RandomDemo.java
+++ src/main/java/RandomDemo.java
@@ -1,27 +1,36 @@
+import java.util.DoubleSummaryStatistics;
 import java.util.OptionalInt;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 public class RandomDemo {
     public static void main(String[] args) {
-        Random r = new Random();
-        int sum = IntStream.generate(() -> r.nextInt(10))
-                .limit(10)
-                .map(n -> {
-                    System.out.println("n = " + n);
-                    return n;
-                })
-                .filter(n -> n % 2 == 0)  // only even numbers
-                .peek(System.out::println)
-                .map(n -> n * 2)
-                .sum();
-        System.out.println(sum);
+        DoubleSummaryStatistics stats =
+                DoubleStream.generate(Math::random)
+                            .limit(100)
+                            .summaryStatistics();
+        System.out.println(stats);
 
-        System.out.println("Filtering integers:");
-        OptionalInt first = IntStream.generate(() -> r.nextInt(10))
-                .peek(System.out::println)
-                .filter(n -> n > 7)
-                .findFirst();
+//        Random r = new Random();
+//        int sum = IntStream.generate(() -> r.nextInt(10))
+//                .limit(10)
+//                .map(n -> {
+//                    System.out.println("n = " + n);
+//                    return n;
+//                })
+//                .filter(n -> n % 2 == 0)  // only even numbers
+//                .peek(System.out::println)
+//                .map(n -> n * 2)
+//                .sum();
+//        System.out.println(sum);
+//
+//        System.out.println("Filtering integers:");
+//        OptionalInt first = IntStream.generate(() -> r.nextInt(10))
+//                .peek(System.out::println)
+//                .filter(n -> n > 7)
+//                .findFirst();
 
     }
 }
--- src/main/java/lazy/LazyStreams.java
+++ src/main/java/lazy/LazyStreams.java
@@ -7,28 +7,27 @@ public class LazyStreams {
     private static final Logger logger = Logger.getLogger(LazyStreams.class.getName());
 
     public static int multByTwo(int n) {
-        System.out.printf("Inside multByTwo with arg %d%n", n);
+        System.out.printf("Inside multByTwo with arg %d (thread %s)%n", n, Thread.currentThread().getName());
         return n * 2;
     }
 
     public static boolean modByThree(int n) {
-        System.out.printf("Inside modByThree with arg %d%n", n);
+        System.out.printf("Inside modByThree with arg %d (thread %s)%n", n, Thread.currentThread().getName());
         return n % 3 == 0;
     }
 
     public static void main(String[] args) {
         // multiply numbers between 100 and 200 by 2, then find first n divisible by 3
         int firstEvenDoubleDivBy3 = IntStream.range(100, 200)
-                .filter(n -> n % 3 == 0)
                 .map(n -> n * 2)
+                .filter(n -> n % 3 == 0)
                 .findFirst().orElse(0);
         System.out.println(firstEvenDoubleDivBy3);
 
-
         // Demonstrate laziness using print statements
         firstEvenDoubleDivBy3 = IntStream.range(100, 2_000_000)
-                .filter(LazyStreams::modByThree)
                 .map(LazyStreams::multByTwo)
+                .filter(LazyStreams::modByThree)
                 .findFirst().orElse(0);
         System.out.printf("First even divisible by 3 is %d%n", firstEvenDoubleDivBy3);
     }
