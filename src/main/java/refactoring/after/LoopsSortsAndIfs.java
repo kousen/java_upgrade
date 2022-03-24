e5f67bf08ee42a6d0d08868a43a50c4277e9d692
Ken Kousen <ken.kousen@kousenit.com>
2022-03-24 16:02:28 -0400

    Remove ROOT locale

--- src/main/java/refactoring/after/LoopsSortsAndIfs.java
+++ src/main/java/refactoring/after/LoopsSortsAndIfs.java
@@ -2,18 +2,15 @@ package refactoring.after;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Locale;
 
 public class LoopsSortsAndIfs {
     public static void main(String[] args) {
 
         Arrays.stream("this is an array of strings".split(" "))
                 .filter(s -> s.length() % 2 == 0)
-                .map(s -> s.toUpperCase(Locale.ROOT))
+                .map(String::toUpperCase)
                 .sorted(Comparator.comparingInt(String::length)
                         .thenComparing(Comparator.naturalOrder()))
                 .forEach(System.out::println);
-
-
     }
 }
