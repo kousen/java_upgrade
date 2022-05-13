4d5db289f024355d56ceaf3faac00b3982c096d3
Ken Kousen <ken.kousen@kousenit.com>
2022-05-13 12:01:16 -0500

    Add changes from second talk at NFJS Iowa 2022

--- src/test/java/lambdas/FunctionalTests.java
+++ src/test/java/lambdas/FunctionalTests.java
@@ -4,14 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FunctionalTests {
+    private final Logger logger = Logger.getLogger(FunctionalTests.class.getName());
+
     @Test
     void iterableWithConsumer() {
+        Consumer<String> consolePrint = System.out::println;
+        Consumer<String> consoleLogger = logger::info;
+        // function composition
+        Consumer<String> consumer = consolePrint.andThen(consoleLogger);
+
         List.of("this", "is", "a", "list", "of", "strings")
-                .forEach(System.out::println);
+                //.forEach(System.out::println);
+                .forEach(consumer);
     }
 
     private String getErrorMessage() {
