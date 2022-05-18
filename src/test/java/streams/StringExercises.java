fbe0f68e4dbb857902bff4b580fc6d7278842809
Ken Kousen <ken.kousen@kousenit.com>
2022-05-18 11:59:14 -0400

    Add implementations of the StringExercises

--- src/test/java/streams/StringExercises.java
+++ src/test/java/streams/StringExercises.java
@@ -2,10 +2,10 @@ package streams;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class StringExercises {
     private final List<String> strings = Arrays.asList("this", "is", "a",
@@ -13,7 +13,7 @@ public class StringExercises {
 
     @Test
     public void stringLengthSort_InnerClass() {     // Java 5, 6, 7
-        Collections.sort(strings, new Comparator<String>() {
+        strings.sort(new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
                 return s1.length() - s2.length();
@@ -25,8 +25,13 @@ public class StringExercises {
     @Test
     public void stringLengthSort_lambda() {
         // Use lambda for the Comparator (reverse sort)
+        strings.sort((s1, s2) -> s2.length() - s1.length());
+        System.out.println(strings);
 
         // Use the "sorted" method on Stream
+        strings.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length())
+                .forEach(System.out::println);
     }
 
     private static int compareStrings(String s1, String s2) {
@@ -35,26 +40,60 @@ public class StringExercises {
 
     @Test  // Use a lambda that calls 'compareStrings' directly
     public void stringLengthSort_methodCall() {
+        strings.sort((s1, s2) -> compareStrings(s1, s2));
+        System.out.println(strings);
     }
 
     @Test  // Use a method ref to 'compareStrings'
     public void stringLengthSort_methodRef() {
+        strings.sort(StringExercises::compareStrings);
+        System.out.println(strings);
     }
 
     @Test  // Use Comparator.comparingInt
     public void stringLengthSort_comparingInt() {
+        strings.sort(Comparator.comparingInt(String::length)
+                .thenComparing(Comparator.naturalOrder()));
+        System.out.println(strings);
     }
 
     @Test
     public void demoCollectors() {
         // Get only strings of even length
         // Add them to a LinkedList
+        List<String> evens = strings.stream()
+                .filter(s -> s.length() % 2 == 0)
+                // .collect(Collectors.toCollection(() -> new LinkedList<>()));
+                // .collect(Collectors.toCollection(() -> new ArrayList<>(10)));
+                .collect(Collectors.toCollection(LinkedList::new));
+        System.out.println(evens);
+        System.out.println(evens.getClass().getName());
 
         // Add the strings to a map of string to length
+        Map<String, Integer> map = strings.stream()
+                // .collect(Collectors.toMap(s -> s, String::length));
+                .collect(Collectors.toMap(Function.identity(), String::length));
+        System.out.println(map);
 
         // Filter out nulls, then print even-length strings
+        List<String> stringsWithNulls = Arrays.asList("this", null, "is", "a",
+                "list", null, "of", "strings", null, null);
+
+        List<String> evenLengths = stringsWithNulls.stream()
+                //.filter(s -> s != null && s.length() % 2 == 0)  // short-circuiting logical AND
+                // .filter(s -> s != null)
+                .filter(Objects::nonNull)
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(evenLengths);
 
         // Combine the two predicates and use the result to print non-null, even-length strings
+        Predicate<String> evenFilter = s -> s.length() % 2 == 0;
+        Predicate<String> nullFilter = Objects::nonNull;
+        evenLengths = stringsWithNulls.stream()
+                .filter(nullFilter.and(evenFilter))  // function composition
+                .collect(Collectors.toList());
+        System.out.println(evenLengths);
     }
 
 }
