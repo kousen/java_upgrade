938b43f7c696a1993e98933dc30c7a40b4f1f651
Ken Kousen <ken.kousen@kousenit.com>
2022-05-18 10:02:07 -0400

    Add more tests and comment UsePerson

--- src/main/java/lambdas/UsePerson.java
+++ src/main/java/lambdas/UsePerson.java
@@ -1,29 +1,37 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class UsePerson {
-    @SuppressWarnings({"UnusedAssignment", "Convert2MethodRef"})
+    @SuppressWarnings({"Convert2MethodRef"})
     public static void main(String[] args) {
         List<String> names = Arrays.asList("John", "Paul", "George", "Ringo");
 
+        // old-style (i.e., Java 7-) way
+        List<Person> beatles = new ArrayList<>(); // shared mutable state
+        for (String name : names) {
+            beatles.add(new Person(name));
+        }
+        System.out.println(beatles);
+
+        // newer, functional way
         List<Person> people = names.stream()    // Stream<String>
                 .map(name -> new Person(name))  // Stream<Person>
                 .collect(Collectors.toList());  // Converts Stream<Person> to List<Person>
+        System.out.println(people);
 
-
+        // use a constructor reference to instantiate the Person class
         people = names.stream()
-                .map(Person::new)
+                .map(Person::new) // uses the one-arg constructor
                 .collect(Collectors.toList());
-
         System.out.println(people);
 
         Person[] peopleArray = names.stream()
                 .map(Person::new)
                 .toArray(Person[]::new);
-
         System.out.println(Arrays.toString(peopleArray));
     }
 }
--- src/test/java/lambdas/FileFilterTests.java
+++ src/test/java/lambdas/FileFilterTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,6 +24,7 @@ public class FileFilterTests {
         assertEquals(21, files.length);
     }
 
+    @SuppressWarnings({"Convert2Lambda", "Anonymous2MethodRef"})
     @Test
     void listDirectories_anonInnerClass() {
         File[] directories = root.listFiles(new FileFilter() {
@@ -33,10 +38,12 @@ public class FileFilterTests {
 
     @Test
     void listDirectories_expressionLambda() {
-        File[] directories = root.listFiles(file -> file.isDirectory());
+        // File[] directories = root.listFiles(file -> file.isDirectory());
+        File[] directories = root.listFiles(File::isDirectory);
         assertEquals(13, directories.length);
     }
 
+    @SuppressWarnings({"Convert2MethodRef", "CodeBlock2Expr"})
     @Test
     void listDirectories_blockLambda() {
         File[] directories = root.listFiles(file -> {
@@ -62,5 +69,36 @@ public class FileFilterTests {
     void listJavaSourceFiles_filenameFilter() {
         File[] javaSrc = root.listFiles((dir, name) -> name.endsWith(".java"));
         assertEquals(8, javaSrc.length);
+    }
+
+    @Test
+    void useIterableWithConsumer() {
+        List<String> strings = Arrays.asList("this", "is", "a", "list");
+        // traditional for-each loop
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        // new forEach method that takes a consumer
+        // strings.forEach(string -> System.out.println(string));
+        strings.forEach(System.out::println);
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 2);
+
+        // iterate using key set
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+
+        // iterate using entry set
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        // iterate using biconsumer
+        map.forEach((key, value) -> System.out.println(key + ": " + value));
     }
 }
