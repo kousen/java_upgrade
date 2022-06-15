fae213a3f81c4679d7f74c27892ab83577b7f98d
Ken Kousen <ken.kousen@kousenit.com>
2022-06-14 22:03:16 -0400

    Add tests and default methods exercise

--- src/main/java/lambdas/UsePerson.java
+++ src/main/java/lambdas/UsePerson.java
@@ -1,29 +1,36 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class UsePerson {
-    @SuppressWarnings({"UnusedAssignment", "Convert2MethodRef"})
+    @SuppressWarnings({"Convert2MethodRef"})
     public static void main(String[] args) {
         List<String> names = Arrays.asList("John", "Paul", "George", "Ringo");
 
+        // Create a list of Person objects using an ArrayList
+        List<Person> beatles = new ArrayList<>();  // shared mutable state
+        for (String name : names) {
+            beatles.add(new Person(name));
+        }
+        System.out.println(beatles);
+
+        // Create a list of Person objects using a Stream
         List<Person> people = names.stream()    // Stream<String>
                 .map(name -> new Person(name))  // Stream<Person>
                 .collect(Collectors.toList());  // Converts Stream<Person> to List<Person>
-
+        System.out.println(people);
 
         people = names.stream()
                 .map(Person::new)
                 .collect(Collectors.toList());
-
         System.out.println(people);
 
         Person[] peopleArray = names.stream()
                 .map(Person::new)
                 .toArray(Person[]::new);
-
         System.out.println(Arrays.toString(peopleArray));
     }
 }
