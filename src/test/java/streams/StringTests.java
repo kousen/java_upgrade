1afe20b4c7506d626519d6c473fffa0ddf6aafe2
Ken Kousen <ken.kousen@kousenit.com>
2022-10-14 08:56:44 -0400

    Add String tests

---
+++ src/test/java/streams/StringTests.java
@@ -0,0 +1,107 @@
+package streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringTests {
+    private final List<String> strings = Arrays.asList("this", "is", "a",
+            "list", "of", "strings");
+
+    @Test
+    public void stringLengthSort_InnerClass() {     // Java 5, 6, 7
+        strings.sort(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length() - s2.length();
+            }
+        });
+        System.out.println(strings);
+    }
+
+    @Test
+    public void stringLengthSort_lambda() {
+        // Use lambda for the Comparator (reverse sort)
+        strings.sort((s1, s2) -> s2.length() - s1.length());
+        System.out.println(strings);
+
+        // Use the "sorted" method on Stream
+        strings.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length())
+                .forEach(System.out::println);
+    }
+
+    private static int compareStrings(String s1, String s2) {
+        return s1.length() - s2.length();
+    }
+
+    @Test  // Use a lambda that calls 'compareStrings' directly
+    public void stringLengthSort_methodCall() {
+        strings.stream()
+                .sorted((s1, s2) -> compareStrings(s2, s1))
+                .forEach(System.out::println);
+    }
+
+    @Test  // Use a method ref to 'compareStrings'
+    public void stringLengthSort_methodRef() {
+        strings.stream()
+                .sorted(StringTests::compareStrings)
+                .forEach(System.out::println);
+    }
+
+    @Test  // Use Comparator.comparingInt
+    public void stringLengthSort_comparingInt() {
+        strings.stream()
+                .sorted(Comparator.comparingInt(String::length)
+                        .thenComparing(Comparator.naturalOrder()))
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void demoCollectors() {
+        // Get only strings of even length
+        // Add them to a LinkedList
+        List<String> evens = strings.stream()
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toCollection(LinkedList::new));
+        System.out.println(evens);
+        assertEquals(LinkedList.class, evens.getClass());
+
+        // Add the strings to a map of string to length
+        Map<String, Integer> map = strings.stream()
+                // .collect(Collectors.toMap(s -> s, String::length));
+                .collect(Collectors.toMap(Function.identity(), String::length));
+        System.out.println(map);
+
+        // Filter out nulls, then print even-length strings
+        List<String> stringsWithNulls = Arrays.asList("this", "is", null,
+                "a", null, null, "list", "of", null, "strings");
+        evens = stringsWithNulls.stream()
+                // .filter(s -> s != null && s.length() % 2 == 0)
+                // .filter(s -> s != null)
+                .filter(Objects::nonNull)
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(evens);
+
+        // Combine the two predicates and use the result to print non-null, even-length strings
+        Predicate<String> nonNull = Objects::nonNull;
+        Predicate<String> evenLength = s -> s.length() % 2 == 0;
+
+        Logger logger = Logger.getLogger("StringTests");
+        Consumer<String> consolePrinter = System.out::println;
+        Consumer<String> loggerPrinter = logger::info;
+
+        stringsWithNulls.stream()
+                .filter(nonNull.and(evenLength))
+                .forEach(consolePrinter.andThen(loggerPrinter));
+    }
+
+}
