a6c2538d938ad0814b21b99098c11b01f20d12cf
Ken Kousen <ken.kousen@kousenit.com>
2023-09-26 16:23:30 -0400

    Convert CompanyEmployee to class with explicit methods Converted the CompanyEmployee record to a class and explicitly provided getter methods, equals, hashcode, and toString methods. This makes the code clearer and more flexible to program specific behaviors if needed. Updated getter methods in the Employee interface to account for this change.

--- src/main/java/interfaces/CompanyEmployee.java
+++ src/main/java/interfaces/CompanyEmployee.java
@@ -6,7 +6,16 @@ package interfaces;
 // - automatically generate toString, equals, and hashCode
 // - "getters" match the names of the attributes
 
-public record CompanyEmployee(String first, String last) implements Company, Employee {
+import java.util.Objects;
+
+public final class CompanyEmployee implements Company, Employee {
+    private final String first;
+    private final String last;
+
+    public CompanyEmployee(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
 
     @Override
     public String getName() {
@@ -18,4 +27,36 @@ public record CompanyEmployee(String first, String last) implements Company, Emp
     public void doWork() {
         System.out.println("Converting caffeine into code for $$...");
     }
+
+    @Override
+    public String first() {
+        return first;
+    }
+
+    @Override
+    public String last() {
+        return last;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (CompanyEmployee) obj;
+        return Objects.equals(this.first, that.first) &&
+               Objects.equals(this.last, that.last);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, last);
+    }
+
+    @Override
+    public String toString() {
+        return "CompanyEmployee[" +
+               "first=" + first + ", " +
+               "last=" + last + ']';
+    }
+
 }
