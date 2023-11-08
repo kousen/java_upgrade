c36d920a39dd2a863c26949172ecb3509aa3ffd2
Ken Kousen <ken.kousen@kousenit.com>
2023-11-07 21:25:19 -0700

    Add FileFilterTest class for testing FileFilter usage
    New class 'FileFilterTest.java' was added under the 'lambdas' package. It consists of multiple Junit tests that cover different scenarios of using FileFilter and FilenameFilter interfaces to list files/directories. The tests use a variety of approaches including anonymous inner class, lambda expressions, block lambda, method reference and assigning lambda expression to a variable. This addition is to ensure various ways of filtering files are working correctly.

---
+++ src/test/java/lambdas/FileFilterTest.java
@@ -0,0 +1,78 @@
+package lambdas;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileFilterTest {
+    private final File root = new File("src/main/java");
+
+    @Test
+    void listFiles() {
+        File[] files = root.listFiles();
+        assert files != null;
+        assertEquals(22, files.length);
+    }
+
+    @Test
+    void listDirectories_anonInnerClass() {
+        File[] directories = root.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();
+            }
+        });
+        assert directories != null;
+        assertEquals(14, directories.length);
+    }
+
+    @Test
+    void listDirectories_expressionLambda() {
+        File[] directories = root.listFiles(path -> path.isDirectory());
+        assert directories != null;
+        assertEquals(14, directories.length);
+    }
+
+    @Test
+    void listDirectories_blockLambda() {
+        File[] directories = root.listFiles(path -> {
+            System.out.println("checking path = " + path);
+            return path.isDirectory();
+        });
+        assert directories != null;
+        assertEquals(14, directories.length);
+    }
+
+    @Test
+    void listDirectories_methodReference() {
+        File[] directories = root.listFiles(File::isDirectory);
+        assert directories != null;
+        assertEquals(14, directories.length);
+    }
+
+    @Test
+    void listDirectories_assignToVariable() {
+        FileFilter filter = path -> path.isDirectory();
+        File[] directories = root.listFiles(filter);
+        assert directories != null;
+        assertEquals(14, directories.length);
+    }
+
+    @Test
+    void listJavaSrcFiles_fileFilter() {
+        File[] javaFiles = root.listFiles(path -> path.getName().endsWith(".java"));
+        assert javaFiles != null;
+        assertEquals(8, javaFiles.length);
+    }
+
+    @Test
+    void listJavaSrcFiles_fileNameFilter() {
+        File[] javaFiles = root.listFiles((dir, name) -> name.endsWith(".java"));
+        assert javaFiles != null;
+        assertEquals(8, javaFiles.length);
+    }
+}
