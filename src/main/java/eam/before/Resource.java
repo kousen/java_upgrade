def4271857566ce410401039f479b7d713f43c77
Ken Kousen <ken.kousen@kousenit.com>
2023-11-07 19:39:27 -0700

    Update finalize method, JDK version and Gradle version
    Commented out deprecated finalize method in Resource.java to prevent its unintended usage. Switched from JDK 11 to JDK 17 due to its better performance and new language features. Upgraded Gradle version from 8.3 to 8.4 in gradle-wrapper.properties for taking advantage of recent improvements and bug fixes. Also added junit-platform-launcher dependency in build.gradle to enable the use of JUnit Platform's Launcher API.
    
    These changes have been made to align the project with modern development practices, boost performance, and prepare the codebase for more complex testing scenarios.

--- src/main/java/eam/before/Resource.java
+++ src/main/java/eam/before/Resource.java
@@ -13,9 +13,9 @@ public class Resource {
         System.out.println("op2 called...");
     }
 
-    @SuppressWarnings("deprecation")
-    protected void finalize() {
-        System.out.println("do any cleanup here...");
-    }
+//    @SuppressWarnings("deprecation")
+//    protected void finalize() {
+//        System.out.println("do any cleanup here...");
+//    }
 
 }
