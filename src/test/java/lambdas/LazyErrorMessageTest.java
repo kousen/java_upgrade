57d4afdf4bdda06796537ec3827dc1f5b5e63f4b
Ken Kousen <ken.kousen@kousenit.com>
2023-11-08 22:54:40 -0700

    Rename class and update test cases to enhance readability
    Renamed the class LazyErrorMessage to LazyErrorMessageTest for better readability and to accurately reflect its functionality as a test class. Deleted the original LazyErrorMessageTest and refactored its methods into LazyErrorMessage. The methods have been updated to use lambdas for lazy error message generation, improving their efficiency. The changes are meant to enhance code readability and promote efficient use of resources.

--- src/test/java/lambdas/LazyErrorMessageTest.java
+++ src/test/java/lambdas/LazyErrorMessageTest.java
@@ -2,24 +2,35 @@ package lambdas;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.Logger;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SuppressWarnings("Convert2MethodRef")
 public class LazyErrorMessageTest {
+    private final Logger logger = Logger.getLogger(LazyErrorMessageTest.class.getName());
 
     private String getErrorMessage() {
-        System.out.println("Generating error message...");
-        return "x should be true";
+        System.out.println("Calculating error message...");
+        return "This is the error message";
+    }
+
+    private String getLogMessage() {
+        System.out.println("Calculating log message...");
+        return "This is the log message";
     }
 
     @Test
-    void assertArgIsTrue() {
+    void eagerErrorMessage() {
         boolean x = true;
-        assertTrue(x, getErrorMessage()); // 2nd arg is a String
+        logger.fine(getLogMessage());
+        assertTrue(x, getErrorMessage());  // eagerly creates the error message
     }
 
     @Test
-    void assertArgIsTrue_lazyErrorMessage() {
+    void lazyErrorMessage() {
         boolean x = true;
-        assertTrue(x, () -> getErrorMessage());  // 2nd arg is a Supplier<String>
+        logger.fine(() -> getLogMessage());  // Supplier<String>
+        assertTrue(x, () -> getErrorMessage()); // does NOT create the error message
     }
 }
