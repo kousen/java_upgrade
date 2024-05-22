4b5808e9e036546c81c995aad555a777a9a567db
Ken Kousen <ken.kousen@kousenit.com>
2024-05-22 10:03:20 -0400

    Update sorting tests and refactor equality checks in Golfer class
    The commit introduces several test cases for the StringExercises class, specifically focusing on different sorting methods, and collections with the Java Stream API. Changes also include refactoring the equality checks in the Golfer class. Instead of manual checks for null and equality, the java.util.Objects API has been leveraged for checking object equality.

--- src/test/java/streams/StringExercises.java
+++ src/test/java/streams/StringExercises.java
@@ -2,10 +2,12 @@ package streams;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class StringExercises {
     private final List<String> strings = Arrays.asList("this", "is", "a",
@@ -25,8 +27,15 @@ public class StringExercises {
     @Test
     public void stringLengthSort_lambda() {
         // Use lambda for the Comparator (reverse sort)
+        strings.sort((s1, s2) -> s2.length() - s1.length());
+        System.out.println(strings);
 
         // Use the "sorted" method on Stream
+        List<String> sorted = strings.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length())
+                .toList();
+        System.out.println(sorted);
+        System.out.println(strings);
     }
 
     private int compareStrings(String s1, String s2) {
@@ -35,29 +44,66 @@ public class StringExercises {
 
     @Test  // Use a lambda that calls 'compareStrings' directly
     public void stringLengthSort_methodCall() {
-
+        List<String> sorted = strings.stream()
+                .sorted((s1, s2) -> compareStrings(s1, s2))
+                .toList();
+        System.out.println(sorted);
     }
 
     @Test  // Use a method ref to 'compareStrings'
     public void stringLengthSort_methodRef() {
-
+        List<String> sorted = strings.stream()
+                .sorted(this::compareStrings)
+                .toList();
+        System.out.println(sorted);
     }
 
     @Test  // Use Comparator.comparingInt
     public void stringLengthSort_comparingInt() {
-
+        List<String> sorted = strings.stream()
+                .sorted(Comparator.comparingInt(String::length)
+                        .thenComparing(Comparator.naturalOrder()))
+                .toList();
+        System.out.println(sorted);
     }
 
     @Test
     public void demoCollectors() {
         // Get only strings of even length
         // Add them to a LinkedList
+        Collection<String> evenLengthStrings = strings.stream()
+                .filter(s -> s.length() % 2 == 0)
+                .collect(Collectors.toCollection(LinkedList::new));
+        System.out.println(evenLengthStrings);
+        System.out.println(evenLengthStrings.getClass().getName());
 
         // Add the strings to a map of string to length
+        Map<String, Integer> map = strings.stream()
+                //.collect(Collectors.toMap(Function.identity(), String::length));
+                .collect(Collectors.toMap(s -> s, String::length));
+        System.out.println(map);
 
         // Filter out nulls, then print even-length strings
+        List<String> stringsWithNulls = Arrays.asList("this", null, "is", null, "a", "test", null);
+        List<String> evens = stringsWithNulls.stream()
+                //.peek(System.out::println)
+                //.filter(s -> s != null && s.length() % 2 == 0) // guard condition
+                //.filter(s -> s != null)
+                .filter(Objects::nonNull)
+                .filter(s -> s.length() % 2 == 0)
+                .toList();
+        System.out.println(evens);
 
         // Function composition
+        Logger logger = Logger.getLogger(StringExercises.class.getName());
+        Consumer<String> log = logger::info;
+        Consumer<String> print = System.out::println;
+
+        Predicate<String> nonNull = Objects::nonNull;
+        Predicate<String> evenLength = s -> s.length() % 2 == 0;
+        stringsWithNulls.stream()
+                .filter(nonNull.and(evenLength))
+                .forEach(log.andThen(print));
 
         // Combine the two predicates and use the result to print non-null, even-length strings
 
