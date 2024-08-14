28bbf684f47f68cd3402e3f231eb77bdd8168232
kousen <ken.kousen@kousenit.com>
2024-08-14 10:53:54 -0400

    Added more tests and lazy logging demo

--- src/test/java/lambdas/FileFilterTest.java
+++ src/test/java/lambdas/FileFilterTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class FileFilterTest {
@@ -40,6 +42,13 @@ public class FileFilterTest {
     }
 
     @Test
+    void listDirectories_method_reference() {
+        File[] directories = root.listFiles(File::isDirectory);
+        assert directories != null;
+        assertEquals(14, directories.length);
+    }
+
+    @Test
     void listDirectories_blockLambda() {
         File[] directories = root.listFiles(path -> {
             return path.isDirectory();
@@ -54,5 +63,27 @@ public class FileFilterTest {
         File[] directories = root.listFiles(filter);
         assert directories != null;
         assertEquals(14, directories.length);
+    }
+
+    @Test
+    void expect_exception() {
+        String x = null;
+        Exception ex = assertThrows(NullPointerException.class,
+                () -> x.length());
+        System.out.println(ex.getMessage());
+    }
+
+    @Test
+    void list_directories_java_src_files_filenamefilter() {
+        File[] directories = root.listFiles((dir, name) -> name.endsWith(".java"));
+        assertNotNull(directories);
+        assertEquals(8, directories.length);
+    }
+
+    @Test
+    void list_directories_java_src_files_filefilter() {
+        File[] directories = root.listFiles(path -> path.getName().endsWith(".java"));
+        assertNotNull(directories);
+        assertEquals(8, directories.length);
     }
 }
--- src/test/java/lambdas/LazyErrorMessageTest.java
+++ src/test/java/lambdas/LazyErrorMessageTest.java
@@ -2,12 +2,20 @@ package lambdas;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.Logger;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LazyErrorMessageTest {
+    private final Logger log = Logger.getLogger(this.getClass().getName());
 
     private String getErrorMessage() {
         System.out.println("Generating error message...");
+        return "x should be true";
+    }
+
+    private String getLogMessage() {
+        System.out.println("Generating log message...");
         return "x should be true";
     }
 
@@ -15,11 +23,13 @@ public class LazyErrorMessageTest {
     void assertArgIsTrue() {
         boolean x = true;
         assertTrue(x, getErrorMessage()); // 2nd arg is a String
+        log.fine(getLogMessage());
     }
 
     @Test
     void assertArgIsTrue_lazyErrorMessage() {
         boolean x = true;
         assertTrue(x, () -> getErrorMessage());  // 2nd arg is a Supplier<String>
+        log.fine(() -> getLogMessage());
     }
 }
