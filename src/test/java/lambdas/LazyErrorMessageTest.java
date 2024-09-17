42ccad5847ec9191366c71ff6b83f57f3b8c13cc
Ken Kousen <ken.kousen@kousenit.com>
2024-09-17 10:49:04 -0400

    Add logging to tests in LazyErrorMessageTest
    Introduced the usage of `Logger` to provide additional log messages in test methods. The log messages are generated lazily in the `assertArgIsTrue_lazy` method to avoid unnecessary computation.

--- src/test/java/lambdas/LazyErrorMessageTest.java
+++ src/test/java/lambdas/LazyErrorMessageTest.java
@@ -2,24 +2,34 @@ package lambdas;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.Logger;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LazyErrorMessageTest {
+    private final Logger logger = Logger.getLogger(LazyErrorMessageTest.class.getName());
 
     private String getErrorMessage() {
         System.out.println("Generating error message...");
         return "x should be true";
     }
 
-    @Test
-    void assertArgIsTrue() {
-        boolean x = true;
-        assertTrue(x, getErrorMessage()); // 2nd arg is a String
+    private String getLogMessage() {
+        System.out.println("Generating log message...");
+        return "log message";
     }
 
     @Test
-    void assertArgIsTrue_lazyErrorMessage() {
+    void assertArgIsTrue_eager() {
+        boolean x = true;
+        assertTrue(x, getErrorMessage()); // 2nd arg is a String
+        logger.fine(getLogMessage());
+    }
+
+    @Test
+    void assertArgIsTrue_lazy() {
         boolean x = true;
         assertTrue(x, () -> getErrorMessage());  // 2nd arg is a Supplier<String>
+        logger.fine(() -> getLogMessage());
     }
 }
